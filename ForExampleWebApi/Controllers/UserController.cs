b34fd37a4d5ec306e725c10cd3907150b9fbc092
Hamed <hamed_abutorabi@yahoo.com>
2023-05-19 17:11:06 +0200

    add jwt token for bearer authentication

--- ForExampleWebApi/Controllers/UserController.cs
+++ ForExampleWebApi/Controllers/UserController.cs
@@ -1,8 +1,12 @@
 ﻿using ForExampleWebApi.Data;
 using ForExampleWebApi.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 namespace ForExampleWebApi.Controllers
 {
@@ -10,10 +14,43 @@ namespace ForExampleWebApi.Controllers
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
+        private readonly IConfiguration _config;
         private readonly UserDbContext _context;
-        public UserController(UserDbContext context)
+        public UserController(UserDbContext context, IConfiguration configuration)
         {
             _context = context;
+            _config = configuration;
+        }
+        private User AuthenticateUser(User user)
+        {
+            var foundUser = _context.Users.Where(a => a.UserName == user.UserName && a.PasswordHash == user.PasswordHash).FirstOrDefault();
+            if (foundUser != null)
+            {
+                return foundUser;
+            }
+            return null;
+        }
+        private string GenerateToken(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Audience"], null,
+                expires: DateTime.Now.AddDays(1),signingCredentials:credentials);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            IActionResult response = Unauthorized();
+            var user_ = AuthenticateUser(user);
+            if (user_ != null)
+            {
+                var token = GenerateToken(user_);
+                response = Ok(new { token = token });
+            }
+            return response;
         }
         [HttpGet]
         public async Task<IEnumerable<User>> Get() => await _context.Users.ToListAsync();
@@ -35,7 +72,7 @@ namespace ForExampleWebApi.Controllers
         }
 
         [HttpPut]
-
+        [Authorize]
         public async Task<IActionResult> ChangePassword(int id, string newPasswordHash)
         {
             var user = _context.Users.Find(id);
--- ForExampleWebApi/Program.cs
+++ ForExampleWebApi/Program.cs
@@ -1,7 +1,24 @@
 using ForExampleWebApi.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer( options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt.Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt.Key"]))
+        };
+    });
 
 // Add services to the container.
 
@@ -22,7 +39,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
